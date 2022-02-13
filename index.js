45f7eaf72e141e899bab8a04848ad7de7d831960
Jerovsky Slava <mako11.83@mail.ru>
2022-02-13 12:19:48 +0300

    my homework

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-// Задача 1.
-// Реализуйте reverse(), которая принимает на вход массив и располагает элементы внутри него в обратном порядке. Решение этой задачи подразумевает самостоятельную реализацию функции без использования встроенного метода reverse().
+// Задача 2.
+// Панграмма — это предложение, в котором каждая буква алфавита встречается хотя бы по одному разу. Например, предложение "The quick brown fox jumps over the lazy dog" является панграммой, поскольку в нем хотя бы один раз используются буквы от A до Z (регистр значения не имеет).
+// Напишите функцию, которая возвращает true или false в зависимости от того, является она панграммой или нет. Не обращайте внимания на цифры и знаки препинания. Работаем только с латиницей.
 
-// const names = ['john', 'smith', 'karl']; // ['john', 'smith', 'karl']
+// console.log(isPangram("The quick brown fox jumps over the lazy dog")) // true
+// console.log(isPangram("This is not a pangram")) // false
 
-
-const names = ['john', 'smith', 'karl'];
-let newNames = [];
-function reverse(names){
-for (let i = names.length - 1; i> -1; i--){
-newNames.push(names[i]);
+let letters = 'abcdefghijklmnopqrstuywxyz'
+function isPangrams(letter) {
+for (let i = 0; i < letters.length; i++){
+if(letter.indexOf(letters[i]) === -1){
+return 'false';
 }
-return newNames;
 }
-console.log(reverse(names));
+return 'true';
+}
+console.log(isPangrams("The quick brown fox jumps over the lazy dog"));
+console.log(isPangrams("This is not a pangram"))
